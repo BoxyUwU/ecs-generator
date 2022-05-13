79adec87057de0c6011bdbceefb3d45a9a18bc0e
Ellen <supbscripter@gmail.com>
2022-05-13 03:03:42 +0100

    make bench output pretty

--- benches/benches/lib.rs
+++ benches/benches/lib.rs
@@ -4,18 +4,25 @@ use ecs_generator::WorldConstructArgs;
 mod single_iter;
 
 fn bench_single_iter(c: &mut Criterion) {
-    let mut group = c.benchmark_group("single_iter");
-    group.warm_up_time(std::time::Duration::from_millis(500));
-    group.measurement_time(std::time::Duration::from_secs(4));
     for entity_count in [100, 400, 800, 4000, 8000] {
-        for archetype_count in [25, 100, 200, 500, 1000] {
-            let divd_count = archetype_count / 4;
-            for matched_archetype_count in [
-                1,
-                divd_count,
-                divd_count * 2,
-                divd_count * 3,
-                archetype_count,
+        let mut group = c.benchmark_group(format!("single_iter/e{}", entity_count));
+        group.warm_up_time(std::time::Duration::from_millis(500));
+        group.measurement_time(std::time::Duration::from_secs(5));
+
+        for (archetype_count, line_name) in [
+            (25, "A25"),
+            (100, "B100"),
+            (200, "C200"),
+            (500, "D500"),
+            (1000, "E1000"),
+        ] {
+            let divd_count = archetype_count / 5;
+            for (matched_archetype_count, id) in [
+                (divd_count, "20"),
+                (divd_count * 2, "40"),
+                (divd_count * 3, "60"),
+                (divd_count * 4, "80"),
+                (archetype_count, "100"),
             ] {
                 let args = WorldConstructArgs {
                     matched_archetype_count,
@@ -23,13 +30,11 @@ fn bench_single_iter(c: &mut Criterion) {
                     entity_count,
                 };
 
-                group.bench_function(
-                    &format!(
-                        "matched_arch_count:{}, arch_count:{}, entity_count:{}",
-                        matched_archetype_count, archetype_count, entity_count
-                    ),
-                    |b| {
-                        let mut bench = single_iter::Benchmark::new(args);
+                group.bench_with_input(
+                    BenchmarkId::new(format!("{}", line_name), id),
+                    &args,
+                    |b, args| {
+                        let mut bench = single_iter::Benchmark::new(args.clone());
                         b.iter(move || bench.run());
                     },
                 );
